eab1b59ca42ce5ebe36a30b7631e77821323f648
kel08kel <163026035+kel08kel@users.noreply.github.com>
2025-02-28 14:31:26 -0300

    Add files via upload
    28/02/2025
    Copiei os códigos do html da aula 4 e comecei a copia os codigos do scripit.js, tambem comecei a copiar os códigos no caderno.

---
+++ scripit.js
@@ -0,0 +1,9 @@
+document.addEventListener ('DOMContentLoaded' function (){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener ('clik', function (){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+});
