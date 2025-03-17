e437d8a34e8dcc21f1b4e0fb95dcf607a9a20bed
kel08kel <163026035+kel08kel@users.noreply.github.com>
2025-03-17 14:29:35 -0300

    Add files via upload

--- scripit.js
+++ scripit.js
@@ -1,5 +1,6 @@
 document.addEventListener ('DOMContentLoaded' function (){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener ('clik', function (){
@@ -7,3 +8,11 @@ document.addEventListener ('DOMContentLoaded' function (){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
 });
+
+  diminuiFonteBotao.addEventListener('click, function'(){
+    tamanhoAtualFonte -=0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+  
+   });
+
+  });
